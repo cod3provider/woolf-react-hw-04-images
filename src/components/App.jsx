283f07b417fc9c747e5cd64e6d12da63ba4f072d
Ilya Bondarchuk <cod3provider@gmail.com>
2024-02-28 11:41:04 +0100

    refactored App component, moved to functional component, added useEffect for response, changed classes state to useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -11,125 +11,132 @@ import Loader from './Loader/Loader';
 
 import { getImages, PER_PAGE, responseImages } from '../api/API';
 
-class App extends Component {
-  state = {
-    images: [],
-    query: '',
-    page: 1,
-    isLoading: false,
-    error: null,
-    largeImageURL: '',
-    tags: '',
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isShowModal, setIsShowModal] = useState(false);
+  const [error, setError] = useState(null);
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [tags, setTags] = useState('');
+  const [totalPages, setTotalPages] = useState(0);
+  console.log(query);
+
+  // state = {
+  //   images: [],
+  //   query: '',
+  //   page: 1,
+  //   isLoading: false,
+  //   error: null,
+  //   largeImageURL: '',
+  //   tags: '',
+  // };
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   const { page, query } = this.state;
+  // }
+
+  const searchImages = query => {
+    setQuery(query);
+    setImages([]);
+    setPage(1);
+    setIsShowModal(false);
+    // query,
+    // images: [],
+    // page: 1,
+    // isShowModal: false,
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    const { page, query } = this.state;
-    if (prevState.query !== query || prevState.page !== page) {
-      this.fetchImages();
+  useEffect(() => {
+    if (!query) {
+      return;
     }
-  }
 
-  searchImages = query => {
-    this.setState({
-      query,
-      images: [],
-      page: 1,
-      isShowModal: false,
-    });
-  };
+    const fetchImages = async () => {
+      try {
+        setIsLoading(true);
 
-  async fetchImages() {
-    try {
-      this.setState({ isLoading: true });
+        const { hits, totalHits } = await getImages(query, page);
+        const newImages = responseImages(hits);
 
-      const { query, page } = this.state;
-      const { hits, totalHits } = await getImages(query, page);
-      const pictures = responseImages(hits);
-
-      if (pictures.length === 0) {
-        toast.warn('Images not found');
+        if (newImages.length === 0) {
+          toast.warn('Images not found');
+        }
+        if (page === 1) {
+          setTotalPages(Math.ceil(totalHits / PER_PAGE));
+        }
+        setImages(prevImages => [...prevImages, ...newImages]);
       }
-      if (page === 1) {
-        this.setState({ totalPages: Math.ceil(totalHits / PER_PAGE) });
+      catch (err) {
+        setError(err.message);
+        toast.error('Something went wrong.');
       }
-      this.setState(({ images }) => ({
-        images: [...images, ...pictures],
-      }));
-    }
-    catch (err) {
-      this.setState({ error: err.message });
-      toast.error('Something went wrong.');
-    }
-    finally {
-      this.setState({ isLoading: false });
-    }
+      finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchImages();
+  }, [query, page]);
+
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const handleToggleModal = () => {
+    setIsShowModal(!isShowModal);
   };
 
-  handleToggleModal = () => {
-    this.setState(({ isShowModal }) => ({
-        isShowModal: !isShowModal,
-      }),
-    );
+  const handleModalClick = (largeImageURL, tags) => {
+    setLargeImageURL(largeImageURL);
+    setTags(tags);
+    handleToggleModal();
   };
 
-  handleModalClick = (largeImageURL, tags) => {
-    this.setState({
-      largeImageURL,
-      tags,
-    });
-    this.handleToggleModal();
-  };
+  // const { searchImages, handleLoadMore, handleModalClick, handleToggleModal } = this;
+  // const { images, page, totalPages, isShowModal, largeImageURL, tags, isLoading } = this.state;
 
-  render() {
-    const { searchImages, handleLoadMore, handleModalClick, handleToggleModal } = this;
-    const { images, page, totalPages, isShowModal, largeImageURL, tags, isLoading } = this.state;
-    const isMoreImages = images.length > 0 && page !== totalPages;
+  const isMoreImages = images.length > 0 && page !== totalPages;
 
-    return (
-      <>
-        <Searchbar onSubmit={searchImages} />
-        <Container>
-          <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="colored"
+  return (
+    <>
+      <Searchbar onSubmit={searchImages} />
+      <Container>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
+        <ImageGallery
+          images={images}
+          onOpenModal={handleModalClick}
+        />
+        {isLoading && <Loader />}
+        {isMoreImages &&
+          <Button
+            text="Load more"
+            onClick={handleLoadMore}
           />
-          <ImageGallery
-            images={images}
-            onOpenModal={handleModalClick}
+        }
+        {isShowModal &&
+          <Modal
+            handleModalClick={handleToggleModal}
+            image={largeImageURL}
+            alt={tags}
           />
-          {isLoading && <Loader />}
-          {isMoreImages &&
-            <Button
-              text="Load more"
-              onClick={handleLoadMore}
-            />
-          }
-          {
-            isShowModal &&
-            <Modal
-              handleModalClick={handleToggleModal}
-              image={largeImageURL}
-              alt={tags}
-            />
-          }
-        </Container>
-      </>
-    );
-  }
-}
+        }
+      </Container>
+    </>
+  );
+};
+
 
 export default App;
