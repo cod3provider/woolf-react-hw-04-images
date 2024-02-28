a9a082798c0246a9165868045638a919ccd14f5a
Ilya Bondarchuk <cod3provider@gmail.com>
2024-02-28 11:39:17 +0100

    refactored Searchbar component, moved to functional component

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,52 +1,41 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import icon from '../../images/icons/magnifying-glass.svg';
 
 import s from './Searchbar.module.css';
 
-class Searchbar extends Component {
-  state = {
-    query: '',
+const Searchbar = ({ onSubmit }) => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = e => {
+    const { value } = e.target;
+    setQuery(value.toLowerCase());
   };
 
-  handleChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value.toLowerCase() });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-
-    const { onSubmit } = this.props;
-    const { query } = this.state;
     onSubmit(query);
-    //   reset();
   };
 
-  render() {
-    const { handleSubmit, handleChange } = this;
-    const { query } = this.state;
-
-    return (
-      <div className={s.searchbar}>
-        <form onSubmit={handleSubmit} className={s.form}>
-          <input
-            className={s.input}
-            onChange={handleChange}
-            type="text"
-            name="query"
-            value={query}
-            placeholder="Search images and photos"
-            autoComplete="off"
-            autoFocus
-          />
-          <button className={s.button} type="submit">
-            <img src={icon} alt="icon" />
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={s.searchbar}>
+      <form onSubmit={handleSubmit} className={s.form}>
+        <input
+          className={s.input}
+          onChange={handleChange}
+          type="text"
+          name="query"
+          value={query}
+          placeholder="Search images and photos"
+          autoComplete="off"
+          autoFocus
+        />
+        <button className={s.button} type="submit">
+          <img src={icon} alt="icon" />
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Searchbar;
