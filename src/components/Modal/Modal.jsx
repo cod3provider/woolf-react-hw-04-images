ffc1ef6b23ede4b8cb272732ed6e69a3d54ced64
Ilya Bondarchuk <cod3provider@gmail.com>
2024-02-28 11:36:03 +0100

    refactored Modal component, moved to functional component, added useEffect hook

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,38 +1,33 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 import s from './Modal.module.css';
 
-class Modal extends Component {
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleCloseModal);
-  }
+const Modal = ({ image, alt, handleModalClick }) => {
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleCloseModal);
-  }
-
-  handleCloseModal = e => {
-    const { handleModalClick } = this.props;
+  const handleCloseModal = e => {
     if (e.target === e.currentTarget || e.code === 'Escape') {
       handleModalClick();
     }
   };
 
-  render() {
-    const { image, alt } = this.props;
-    const { handleCloseModal } = this;
+  useEffect(() => {
+    document.addEventListener('keydown', handleCloseModal);
 
-    return (
-      createPortal(
-        <div className={s.overlay} onClick={handleCloseModal}>
-          <div className={s.modal}>
-            <img src={image} alt={alt} />
-          </div>
-        </div>, document.body,
-      )
-    );
-  }
-}
+    return () => {
+      document.removeEventListener('keydown', handleCloseModal);
+    };
+  }, []);
+
+  return (
+    createPortal(
+      <div className={s.overlay} onClick={handleCloseModal}>
+        <div className={s.modal}>
+          <img src={image} alt={alt} />
+        </div>
+      </div>, document.body,
+    )
+  );
+};
 
 export default Modal;
